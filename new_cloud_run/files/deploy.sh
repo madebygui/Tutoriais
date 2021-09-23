09b210e5c2392d15170e16edeb52bdc634d61a7b
Guilherme Miranda <guilhermemiranda.ti@gmail.com>
2021-09-23 15:38:40 -0300

    Update deploy.sh

--- new_cloud_run/files/deploy.sh
+++ new_cloud_run/files/deploy.sh
@@ -1,10 +1,39 @@
-#!/bin/bash
-gcloud config configurations activate nome_docker
-gcloud builds submit --tag gcr.io/nome_projeto/nome_docker
-gcloud run deploy nome_docker \
---image gcr.io/nome_projeto/nome_docker \
---memory 1Gi \
---port 80 \
---platform managed \
---region us-central1 \
---allow-unauthenticated
+####################################
+########### VARIABLES ##############
+####################################
+GCP_PROJECT="codigo-projeto-gcp"
+SERVICE_NAME="nome-meu-projeto"
+REGION="us-central1"
+
+####################################
+###### GENERATED VARIABLES #########
+####################################
+VERSION="$(yarn run env | grep npm_package_version | sed 's/"npm_package_version": "//g' | sed 's/",//g' | sed 's/ //g')"
+IMAGE_NAME="gcr.io/$GCP_PROJECT/$SERVICE_NAME"
+IMAGE_NAME_VERSION="$IMAGE_NAME:$VERSION"
+IMAGE_NAME_LATEST="$IMAGE_NAME:latest"
+
+####################################
+############# BUILD ################
+####################################
+echo "Create Docker image via 'Cloud Build' ..."
+gcloud builds submit  \
+  --tag "$IMAGE_NAME_VERSION" \
+  --project $GCP_PROJECT
+
+####################################
+############ TAGGING ###############
+####################################
+echo "Add tags '$IMAGE_NAME_LATEST' and '$IMAGE_NAME_VERSION' to docker image..."
+gcloud container images add-tag "$IMAGE_NAME_VERSION" $IMAGE_NAME_LATEST --quiet
+
+####################################
+########### DEPLOYMENT #############
+####################################
+echo "Deploying Cloud Run Service '$SERVICE_NAME' to '$GCP_PROJECT' in '$REGION'  🚀 🍀"
+gcloud run deploy $SERVICE_NAME \
+  --image "$IMAGE_NAME_VERSION" \
+  --project $GCP_PROJECT \
+  --platform managed \
+  --region $REGION \
+  --allow-unauthenticated
